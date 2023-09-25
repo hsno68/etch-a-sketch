eda543d573e776dfd251994f45ebc99acb79d81e
haiseno <haise.zhao@gmail.com>
2023-09-25 00:23:11 -0700

    Create 16x16 grid with layout styling

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const parentContainer = document.querySelector(".parent-container");
+
+for (let i = 0; i < 16; i++) {
+  const childContainer = document.createElement("div");
+  childContainer.classList.add("child-container");
+  parentContainer.appendChild(childContainer);
+  for (let j = 0; j < 16; j++) {
+    const child = document.createElement("div");
+    child.classList.add("child");
+    childContainer.appendChild(child);
+  }
+}
