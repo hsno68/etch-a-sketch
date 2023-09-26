642dfcc5f0b53614f4ae488bb6491411e8cf760a
haiseno <haise.zhao@gmail.com>
2023-09-25 21:51:25 -0700

    Add buttons to generate and clear grid
    Put the grid generation loop in a function and link as a callback
    to a generate button. The function clears existing grid first, then
    takes in a user input number n to generate a n x n grid. The comparison
    operator in the for loop converts the user input string to a number,
    if valid.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 const parentContainer = document.querySelector(".parent-container");
+const generateButton = document.querySelector(".generate-button");
+const clearButton = document.querySelector(".clear-button");
 
-for (let i = 0; i < 16; i++) {
-  const childContainer = document.createElement("div");
-  childContainer.classList.add("child-container");
-  parentContainer.appendChild(childContainer);
-  for (let j = 0; j < 16; j++) {
-    const child = document.createElement("div");
-    child.classList.add("child");
-    child.addEventListener("mouseenter", () => child.classList.add("hovered"));
-    childContainer.appendChild(child);
+generateButton.addEventListener("click", createGrid);
+clearButton.addEventListener("click", () => parentContainer.replaceChildren());
+
+function createGrid() {
+  parentContainer.replaceChildren();
+  const gridSize = prompt("Input a size for the grid");
+  for (let i = 0; i < gridSize; i++) {
+    const childContainer = document.createElement("div");
+    childContainer.classList.add("child-container");
+    parentContainer.appendChild(childContainer);
+    for (let j = 0; j < gridSize; j++) {
+      const child = document.createElement("div");
+      child.classList.add("child");
+      child.addEventListener("mouseenter", () => child.classList.add("hovered"));
+      childContainer.appendChild(child);
+    }
   }
 }
